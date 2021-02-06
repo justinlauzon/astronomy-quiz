c22cec6d34072bd262a7991d81a4e106e668730b
justinlauzon <69997850+justinlauzon@users.noreply.github.com>
2021-02-06 18:56:53 -0500

    enable responsive homepage on mobile

--- js/game.js
+++ js/game.js
@@ -238,7 +238,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('end');
+        return window.location.assign('end.html');
     }
 
  
